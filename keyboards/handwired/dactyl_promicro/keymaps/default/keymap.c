ccf184cd8524c7ea5b7beb7f18e65fdb2d1042c8
Zaiming Shi <zaiming.shi@tink.com>
2020-02-25 11:30:37 +0100

    Vim mode direction keys

--- keyboards/handwired/dactyl_promicro/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/default/keymap.c
@@ -14,7 +14,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC  , KC_1 , KC_2   , KC_3    , KC_4   , KC_5   ,                          KC_6 , KC_7 , KC_8    , KC_9   , KC_0    , KC_MINS  ,
-        KC_QUOT , KC_Q , KC_W   , KC_E    , KC_R   , KC_T   ,                          KC_Y , KC_U , KC_I    , KC_O   , KC_P    , KC_EQL   ,
+        KC_EQL  , KC_Q , KC_W   , KC_E    , KC_R   , KC_T   ,                          KC_Y , KC_U , KC_I    , KC_O   , KC_P    , KC_QUOT  ,
         KC_TAB  , KC_A , KC_S   , KC_D    , KC_F   , KC_G   ,                          KC_H , KC_J , KC_K    , KC_L   , KC_SCLN , KC_ENTER ,
         KC_GRV  , KC_Z , KC_X   , KC_C    , KC_V   , KC_B   ,                          KC_N , KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_BSLS  ,
                          KC_LGUI, CTL_B   , KC_SPC ,                                    KC_BSPC , KC_RALT, KC_RGUI,
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              TT(_RAISE),  TT(_LOWER)
     ),
     [_LOWER] = LAYOUT_5x6(
-        KC_ESC  , KC_F1  , KC_F2  , KC_F3  , KC_F4,   KC_F5  ,                         KC_F6 , KC_F7   , KC_F8   , KC_F9   , KC_F10   , KC_F11 ,
-        KC_DEL  , KC_EXLM, KC_AT  , KC_LBRC, KC_RBRC, KC_PIPE,                         KC_1  , KC_2    , KC_UP   , KC_4    , KC_5     , KC_F12 ,
-        KC_QUES , KC_HASH, KC_DLR , KC_LPRN, KC_RPRN, KC_MINS,                         KC_3  , KC_LEFT , KC_DOWN , KC_RGHT , KC_SLSH  , KC_RGUI,
-        KC_TILD , KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_UNDS,                         KC_6  , KC_7    , KC_8    , KC_9    , KC_0     , _______,
+        KC_ESC  , KC_F1  , KC_F2  , KC_F3  , KC_F4,   KC_F5  ,                         KC_F6  , KC_F7   , KC_F8   , KC_F9   , KC_F10   , KC_F11 ,
+        KC_DEL  , KC_EXLM, KC_AT  , KC_LBRC, KC_RBRC, KC_PIPE,                         KC_1   , KC_2    , KC_3    , KC_4    , KC_5     , KC_F12 ,
+        KC_QUES , KC_HASH, KC_DLR , KC_LPRN, KC_RPRN, KC_MINS,                         KC_LEFT, KC_DOWN , KC_UP   , KC_RGHT , KC_SLSH  , KC_RGUI,
+        KC_TILD , KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_UNDS,                         KC_6   , KC_7    , KC_8    , KC_9    , KC_0     , _______,
                            KC_DOT , KC_SLSH,     LG_Z,                                      KC_MINS     , KC_0    , KC_0,
                                                         LG_Y,                       KC_DOT,
                                                              _______,       _______
