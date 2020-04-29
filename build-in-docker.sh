f906e49ccf687e26efb457a669b7ab5916739be0
Zaiming Shi <zaiming.shi@tink.com>
2020-04-29 11:01:46 +0200

    echo flash instruction after build

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -3,4 +3,6 @@
 set -euo pipefail
 
 IMG=qmkfm/base_container
-docker run -it -w /qmk_firmware --rm -v $(pwd):/qmk_firmware $IMG sh -c 'make handwired/dactyl_promicro:default; bash'
+docker run -it -w /qmk_firmware --rm -v $(pwd):/qmk_firmware $IMG sh -c 'make handwired/dactyl_promicro:default'
+echo "done, execute below command to flash"
+echo "sudo avrdude -p atmega32u4 -P /dev/tty.usbmodem* -c avr109 -U flash:w:.build/handwired_dactyl_promicro_default.hex"
