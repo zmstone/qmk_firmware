5c3cd7fffb03630d2181bfa01a5af156bef54b50
Zaiming Shi <zaiming.shi@tink.com>
2020-04-29 10:39:37 +0200

    switch curly brackets and square brackets

--- keyboards/handwired/dactyl_promicro/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/default/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_LOWER] = LAYOUT_5x6(
         _______, KC_F1  , KC_F2  , KC_F3  , KC_F4,   KC_F5  ,                         KC_F6  , KC_F7   , KC_F8   , KC_F9   , KC_F10   , KC_F11 ,
-        KC_DEL , KC_EXLM, KC_AT  , KC_LBRC, KC_RBRC, KC_PIPE,                         KC_1   , KC_2    , KC_3    , KC_4    , KC_5     , KC_F12 ,
+        KC_DEL , KC_EXLM, KC_AT  , KC_LCBR, KC_RCBR, KC_PIPE,                         KC_1   , KC_2    , KC_3    , KC_4    , KC_5     , KC_F12 ,
         _______, KC_HASH, KC_DLR , KC_LPRN, KC_RPRN, KC_MINS,                         KC_LEFT, KC_DOWN , KC_UP   , KC_RGHT , KC_SLSH  , KC_RGUI,
-        KC_LSFT, KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_UNDS,                         KC_6   , KC_7    , KC_8    , KC_9    , KC_0     , _______,
+        KC_LSFT, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_UNDS,                         KC_6   , KC_7    , KC_8    , KC_9    , KC_0     , _______,
                            _______, KC_LALT,     LG_Z,                                      _______, _______ , _______,
                                                         LG_Y,                       _______,
                                                              KC_LCTL,       _______
